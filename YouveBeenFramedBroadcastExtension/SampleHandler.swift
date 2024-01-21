8db0c2fc5da3c782f1eef268cd45f5b7ec1cdeba
Michael Lustig <lustig@knophy.com>
2024-01-20 21:36:24 -0500

    Add logs

--- YouveBeenFramedBroadcastExtension/SampleHandler.swift
+++ YouveBeenFramedBroadcastExtension/SampleHandler.swift
@@ -8,20 +8,31 @@
 import ReplayKit
 
 class SampleHandler: RPBroadcastSampleHandler {
+  
+  override func broadcastAnnotated(withApplicationInfo applicationInfo: [AnyHashable : Any]) {
+      // Handle the application info
+      if let appInfo = applicationInfo as? [String: Any] {
+          print("Received app info: \(appInfo)")
+      }
+  }
 
     override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
-        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional. 
+        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
+      print("broadcastStarted")
     }
     
     override func broadcastPaused() {
         // User has requested to pause the broadcast. Samples will stop being delivered.
+      print("broadcastPaused")
     }
     
     override func broadcastResumed() {
         // User has requested to resume the broadcast. Samples delivery will resume.
+      print("broadcastResumed")
     }
     
     override func broadcastFinished() {
+      print("broadcastFinished")
         // User has requested to finish the broadcast.
     }
     
