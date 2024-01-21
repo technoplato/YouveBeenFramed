99cac79ea377e37dcf94a10f3a14b6b44a05dfad
Michael Lustig <lustig@knophy.com>
2024-01-20 21:13:25 -0500

    Initial Commit

---
+++ YouveBeenFramed/YouveBeenFramedApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouveBeenFramedApp.swift
+//  YouveBeenFramed
+//
+//  Created by laptop on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct YouveBeenFramedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
