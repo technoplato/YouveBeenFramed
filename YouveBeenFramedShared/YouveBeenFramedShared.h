c5f81c14bbb2bba50569e05b69d906c2cefc69aa
Michael Lustig <lustig@knophy.com>
2024-01-20 21:36:32 -0500

    Add Framework

---
+++ YouveBeenFramedShared/YouveBeenFramedShared.h
@@ -0,0 +1,18 @@
+//
+//  YouveBeenFramedShared.h
+//  YouveBeenFramedShared
+//
+//  Created by laptop on 1/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YouveBeenFramedShared.
+FOUNDATION_EXPORT double YouveBeenFramedSharedVersionNumber;
+
+//! Project version string for YouveBeenFramedShared.
+FOUNDATION_EXPORT const unsigned char YouveBeenFramedSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouveBeenFramedShared/PublicHeader.h>
+
+
